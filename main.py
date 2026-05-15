d42dba9b95ee3ea17e3d9763b9a43c43c0736c03
rifaidh <dhiaurrahman22@gmail.com>
2026-05-15 17:36:01 +0700

    Initialize Tkinter app for Maze Game

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import tkinter as tk
+from engine import GameState
+import algorithm
+import maze
 
+class AppController:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Maze Game")
+        self.root.configure(bg="#000000")
+        
+        self.state = GameState()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AppController(root)
+    root.mainloop()
