aeab2c438bd428a5075aaf7f0a2598055af4a04a
rifaidh <dhiaurrahman22@gmail.com>
2026-05-15 21:31:28 +0700

    Add handle_generate method to AppController

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ class AppController:
         
         self.handle_generate()
 
+    def handle_generate(self):
+            new_grid = maze.generate_maze_grid(self.state.rows, self.state.cols, self.state.start_pos, self.state.end_pos)
+            self.state.reset_state(new_grid)
+            self.ui.update_status("MANUAL PLAY: USE ARROW KEYS", self.ui.player_color)
+            self.ui.draw_grid()
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = AppController(root)
