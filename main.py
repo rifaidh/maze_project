1cb5ba52641afb5e73a0672c796741783a3d58f7
rifaidh <dhiaurrahman22@gmail.com>
2026-05-15 21:29:53 +0700

    Integrate MazeUI into AppController

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from engine import GameState
+from ui import MazeUI
 import algorithm
 import maze
 
@@ -10,6 +11,17 @@ class AppController:
         self.root.configure(bg="#000000")
         
         self.state = GameState()
+
+        callbacks = {
+            'generate': self.handle_generate,
+            'solve': self.handle_solve
+        }
+        
+        self.ui = MazeUI(root, self.state, callbacks)
+        
+        self.root.bind("<KeyPress>", self.handle_keypress)
+        
+        self.handle_generate()
 
 if __name__ == "__main__":
     root = tk.Tk()
