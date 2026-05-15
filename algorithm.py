56c731828a8870a7c100e05549242ff08525f958
nanda <nandabaley36@gmail.com>
2026-05-15 16:52:51 +0700

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -3,26 +3,26 @@ from collections import deque
 def bfs_shortest_path(grid, start_pos, end_pos):
     queue = deque([(start_pos, [start_pos])])
     visited = set([start_pos])
-    jalur_ditemukan = []
-    urutan_eksplorasi = []
+    found_path = []
+    exploration_sequence = []
 
     while queue:
-        sekarang, path = queue.popleft()
+        current, path = queue.popleft()
         
-        if sekarang != start_pos and sekarang != end_pos:
-            urutan_eksplorasi.append(sekarang)
+        if current != start_pos and current != end_pos:
+            exploration_sequence.append(current)
         
-        if sekarang == end_pos:
-            jalur_ditemukan = path
+        if current == end_pos:
+            found_path = path
             break
             
-        r, c = sekarang
-        arah = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        r, c = current
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         
-        for dr, dc in arah:
+        for dr, dc in directions:
             nr, nc = r + dr, c + dc
             if grid[nr][nc] == 0 and (nr, nc) not in visited:
                 visited.add((nr, nc))
                 queue.append(((nr, nc), path + [(nr, nc)]))
 
-    return jalur_ditemukan, urutan_eksplorasi
+    return found_path, exploration_sequence
